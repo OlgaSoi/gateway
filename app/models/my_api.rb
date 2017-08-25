aa00e1fbde0241dcbfd426ff6a051b7a75eb099d
Olga Soika <Olga_Soika@epam.com>
2017-08-25 18:36:13 +0300

    RR20 for the presentation 9

--- app/models/my_api.rb
+++ app/models/my_api.rb
@@ -2,10 +2,11 @@ module MyApi
   # this is an "abstract" base class that
   class Base < JsonApiClient::Resource
     # set the api base url in an abstract base class
-    self.site = "http://localhost:3020"
+    self.site = "http://localhost:3030"
   end
 
   class User < Base
+    self.site = "http://localhost:3020"
   end
 
   class SourceProvider < Base
